c72bd2df577dac2332975927e240bf4b0295e15f
Yaroslav Shlapak <yaroslav.shlapak@just-eat.com>
2021-11-04 22:24:18 +0200

    complete section 9

--- StateCodelab/start/src/main/java/com/codelabs/state/todo/TodoViewModel.kt
+++ StateCodelab/start/src/main/java/com/codelabs/state/todo/TodoViewModel.kt
@@ -16,22 +16,43 @@
 
 package com.codelabs.state.todo
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.lifecycle.ViewModel
 
 class TodoViewModel : ViewModel() {
+    private var currentEditPosition: Int by mutableStateOf(-1)
+    var todoItems: SnapshotStateList<TodoItem> = mutableStateListOf()
+        private set
 
-    private var _todoItems = MutableLiveData(listOf<TodoItem>())
-    val todoItems: LiveData<List<TodoItem>> = _todoItems
+    val currentEditItem: TodoItem?
+        get() = todoItems.getOrNull(currentEditPosition)
 
     fun addItem(item: TodoItem) {
-        _todoItems.value = _todoItems.value!! + listOf(item)
+        todoItems.add(item)
+        onEditDone()
     }
 
     fun removeItem(item: TodoItem) {
-        _todoItems.value = _todoItems.value!!.toMutableList().also {
-            it.remove(item)
+        todoItems.remove(item)
+    }
+
+    fun onEditItemSelected(item: TodoItem) {
+        currentEditPosition = todoItems.indexOf(item)
+    }
+
+    fun onEditDone() {
+        currentEditPosition = -1
+    }
+
+    fun onEditItemChange(item: TodoItem) {
+        val currentItem = requireNotNull(currentEditItem)
+        require(currentItem.id == item.id) {
+            "You can only change an item with the same id as currentEditItem"
         }
+        todoItems[currentEditPosition] = item
     }
 }

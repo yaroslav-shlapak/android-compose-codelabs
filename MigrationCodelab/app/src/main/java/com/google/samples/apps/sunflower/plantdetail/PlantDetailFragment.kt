c5b829f0827b0692df24c49971e1c284cb7ae1b9
Yaroslav Shlapak <yaroslav.shlapak@just-eat.com>
2021-11-25 22:14:16 +0200

    Migration to Compose: complete section 10

--- MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
+++ MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
@@ -22,6 +22,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.app.ShareCompat
 import androidx.core.widget.NestedScrollView
@@ -109,9 +110,16 @@ class PlantDetailFragment : Fragment() {
                 }
             }
 
-            composeView.setContent {
-                SunflowerTheme {
-                    PlantDetailDescription(plantDetailViewModel)
+            composeView.apply {
+
+                setViewCompositionStrategy(
+                    ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
+                )
+
+                setContent {
+                    SunflowerTheme {
+                        PlantDetailDescription(plantDetailViewModel)
+                    }
                 }
             }
         }
